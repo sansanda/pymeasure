ed6f16c11accad087266bfeb9b833c1a60cab89f
David <dsanchezsanc@uoc.edu>
2023-06-30 10:37:42 +0200

    Update e2404_test.py

--- pymeasure/instruments/eurotherm/e2404_test.py
+++ pymeasure/instruments/eurotherm/e2404_test.py
@@ -8,17 +8,20 @@ from pymeasure.instruments.eurotherm.eurotherm2404 import CRC16
 
 def main() -> int:
     """Test the communication betwwen PC and the temperature controller Eurotherm 2404"""
-    e2404 = Eurotherm2404('ASRL3::INSTR')
+    e2404 = Eurotherm2404('ASRL1::INSTR')
     e2404.working_setpoint = 1
+    print("Process temperature: ", e2404.process_temperature_value)
+
+
     # e2404.resolution = "full"
-    e2404.automode_enabled = True
-    e2404.working_setpoint = 1
-    e2404.selected_setpoint_target = 75
-    e2404.automode_enabled = True
-    while True:
-        print("Process temperature: ", e2404.process_temperature_value)
-        print("Output power: ", e2404.output_power)
-        time.sleep(1)
+    # e2404.automode_enabled = True
+    # e2404.working_setpoint = 1
+    # e2404.selected_setpoint_target = 75
+    # e2404.automode_enabled = True
+    # while True:
+    #     print("Process temperature: ", e2404.process_temperature_value)
+    #     print("Output power: ", e2404.output_power)
+    #     time.sleep(1)
     return 0
 
 if __name__ == '__main__':
